c097330784dcf1c22914972ce8ddb85a5c805541
Joao Paulo Furtado Silva <joaopaulofurtado@live.com>
2018-09-25 20:32:58 -0700

    Added mysql integration

--- rentalmoose/settings.py
+++ rentalmoose/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'rentalmoose.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rentalmoose',
+        'USER': 'django-admin',
+        'PASSWORD': '32258190',
+        'HOST': '127.0.0.1',
+        'POST': '3306'
     }
 }
 
