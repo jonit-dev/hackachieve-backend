3acb9f9d7cedca88e982db826302aceaca3273b1
Joao Paulo Furtado Silva <joaopaulofurtado@live.com>
2018-09-30 13:16:07 -0700

    protected view

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,96 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rentalmoose.classes.API import *
+from rentalmoose.classes.Validator import *
 
-# Create your views here.
+#for protected views
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+
+# CUSTOM VIEWS =========================== #
+
+
+# ================================================================= #
+#                      DASHBOARD
+# ================================================================= #
+
+# Protected view - dashboard
+@csrf_exempt
+@api_view(['GET'])
+@permission_classes((IsAuthenticated,))
+def user_dashboard(APIView):
+    return API.json_response({
+        'content': "hi"
+    })
+
+
+# ================================================================= #
+#                      REGISTER
+# ================================================================= #
+
+@csrf_exempt
+def user_register(request):
+    if request.method == "POST":
+
+        # get json data
+
+        json_data = API.json_get_data(request)
+
+        # Empty fields valitation =========================== #
+
+        check_user_fields = Validator.are_user_fields_valid(json_data)
+
+        check_user_fields = Validator.are_user_fields_valid(json_data)
+
+        if check_user_fields is not True:
+            return API.json_response({
+                "status": "error",
+                "message": "Error while trying to create your account. The following fields are empty: {}".format(
+                    ", ".join(check_user_fields)),
+                "type": "danger"
+            })
+        elif not Validator.check_password_confirmation(json_data['password'], json_data['passwordConfirmation']):
+            return API.json_response({
+                "status": "error",
+                "message": "Your password does not match its respective password confirmation. Please, try again.",
+                "type": "danger"
+            })
+        elif Validator.check_user_exists(json_data['email']):
+            return API.json_response({
+                "status": "error",
+                "message": "This e-mail is already registered in our system. Please, choose another one and try again.",
+                "type": "danger"
+            })
+
+        else:  # if everything is ok, procceed
+
+            # create user here
+
+            create_user = User.objects.create_user(
+                username=json_data['email'],
+                email=json_data['email'],
+                password=json_data['password'],
+                first_name=json_data['firstName'],
+                last_name=json_data['lastName'],
+                type=1
+            )
+
+        if create_user:
+            return API.json_response({
+                "status": "success",
+                "message": "Your account {} was created successfully! Redirecting...".format(json_data['email']),
+                "type": "success"
+            })
+        else:
+            return API.json_response({
+                "status": "error",
+                "message": "An error occured while trying to create your new user. Please, contact our support.",
+                "type": "danger"
+            })
+
+    else:
+        response = {
+            "error": "Invalid request"
+        }
+    return HttpResponse(json.dumps(response), content_type="application/json")
--- rentalmoose/apiUrls.py
+++ rentalmoose/apiUrls.py
@@ -3,15 +3,21 @@ from django.conf.urls import url, include
 from . import views
 from rest_framework import routers
 
+from apps.users import views as user_views
+
 router = routers.DefaultRouter()
 router.register('provinces', views.ProvinceView)
 router.register('cities', views.CityView)
 router.register('resumes', views.ResumeView)
 router.register('countries', views.CountryView)
 
+
+
+
 urlpatterns = [
 
     url('', include(router.urls)),
-    url('user/create', views.user_register)
+    url('user/create', user_views.user_register),
+    url('user/dashboard', user_views.user_dashboard)
 
 ]
