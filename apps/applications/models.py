7a54ed893a914a3dde12843d7012b986fbf583df
Joao Paulo Furtado Silva <joaopaulofurtado@live.com>
2018-10-28 10:37:25 -0700

    timestamp on application

--- apps/applications/models.py
+++ apps/applications/models.py
@@ -6,11 +6,10 @@ from apps.resumes.models import Resume
 class Application(models.Model):
     resume = models.ManyToManyField(Resume)
     property = models.ManyToManyField(Property)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     @classmethod
     def apply(cls, resume, property):
-
-
         application = Application()
         application.save()
 
