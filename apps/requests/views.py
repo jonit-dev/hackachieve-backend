f93c171f0446a657943535da80430b7f897bf5d8
Joao Paulo Furtado Silva <joaopaulofurtado@live.com>
2018-11-24 21:36:27 -0800

    walkscore prod

--- apps/requests/views.py
+++ apps/requests/views.py
@@ -23,9 +23,25 @@ def walkscore(request, address, lat, lng):
 
     url = "http://api.walkscore.com/score?format=json&address=" + encoded_address + "&lat=" + lat + "&lon=" + lng + "&wsapikey=" + walkscoreAPI
 
+    env = "prod"
 
+    if env == "prod":
+        f = urllib.request.urlopen(url)
+        response = f.read()
 
-    f = urllib.request.urlopen(url)
-    response = f.read()
-
-    return HttpResponse(response, "json")
+        return HttpResponse(response, "json")
+    else:
+        response = {
+            "status": 1,
+            "walkscore": 0,
+            "description": "Car-Dependent",
+            "updated": "2018-10-31 15:38:48.287182",
+            "logo_url": "https://cdn.walk.sc/images/api-logo.png",
+            "more_info_icon": "https://cdn.walk.sc/images/api-more-info.gif",
+            "more_info_link": "https://www.redfin.com/how-walk-score-works",
+            "ws_link": "https://www.walkscore.com/score/13450-.dash.-104-Avenue-.dash.Surrey-BC/lat=53.7266683/lng=-127.6476205/?utm_source=live.com&utm_medium=ws_api&utm_campaign=ws_api",
+            "help_link": "https://www.redfin.com/how-walk-score-works",
+            "snapped_lat": 53.727,
+            "snapped_lon": -127.647
+        }
+        return API.json_response(response)
