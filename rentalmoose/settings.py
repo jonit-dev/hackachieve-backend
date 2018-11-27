e2b7871812ed811fc47f99163a81700c987da6cf
Joao Paulo Furtado Silva <joaopaulofurtado@live.com>
2018-11-26 18:46:21 -0800

    settings remove dotenv code. right place is manage.py

--- rentalmoose/settings.py
+++ rentalmoose/settings.py
@@ -255,32 +255,6 @@ SIMPLE_JWT = {
     # 'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
-
-# ================================================================= #
-#                      DJANGO DOTENV
-# ================================================================= #
-
-#!/usr/bin/env python
-import os
-import sys
-
-import dotenv
-
-
-if __name__ == "__main__":
-    dotenv.read_dotenv()
-
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
 # ================================================================= #
 #                      DJANGO LOGGER
 # ================================================================= #
