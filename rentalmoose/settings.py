2a0bf023a54c9dfd54939fdb934a5e9c5f9fc94c
Joao Paulo Furtado Silva <joaopaulofurtado@live.com>
2018-11-26 18:53:32 -0800

    commented out logger

--- rentalmoose/settings.py
+++ rentalmoose/settings.py
@@ -262,45 +262,45 @@ SIMPLE_JWT = {
 # ================================================================= #
 #                      DJANGO LOGGER
 # ================================================================= #
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        # Include the default Django email handler for errors
-        # This is what you'd get without configuring logging at all.
-        'mail_admins': {
-            'class': 'django.utils.log.AdminEmailHandler',
-            'level': 'ERROR',
-             # But the emails are plain text by default - HTML is nicer
-            'include_html': True,
-        },
-        # Log to a text file that can be rotated by logrotate
-        'logfile': {
-            'class': 'logging.handlers.WatchedFileHandler',
-            'filename': 'rentalmoose/logs/myapp.log'
-        },
-    },
-    'loggers': {
-        # Again, default Django configuration to email unhandled exceptions
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        # Might as well log any errors anywhere else in Django
-        'django': {
-            'handlers': ['logfile'],
-            'level': 'ERROR',
-            'propagate': False,
-        },
-        # Your own app - this assumes all your logger names start with "myapp."
-        'myapp': {
-            'handlers': ['logfile'],
-            'level': 'WARNING', # Or maybe INFO or DEBUG
-            'propagate': False
-        },
-    },
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         # Include the default Django email handler for errors
+#         # This is what you'd get without configuring logging at all.
+#         'mail_admins': {
+#             'class': 'django.utils.log.AdminEmailHandler',
+#             'level': 'ERROR',
+#              # But the emails are plain text by default - HTML is nicer
+#             'include_html': True,
+#         },
+#         # Log to a text file that can be rotated by logrotate
+#         'logfile': {
+#             'class': 'logging.handlers.WatchedFileHandler',
+#             'filename': 'rentalmoose/logs/myapp.log'
+#         },
+#     },
+#     'loggers': {
+#         # Again, default Django configuration to email unhandled exceptions
+#         'django.request': {
+#             'handlers': ['mail_admins'],
+#             'level': 'ERROR',
+#             'propagate': True,
+#         },
+#         # Might as well log any errors anywhere else in Django
+#         'django': {
+#             'handlers': ['logfile'],
+#             'level': 'ERROR',
+#             'propagate': False,
+#         },
+#         # Your own app - this assumes all your logger names start with "myapp."
+#         'myapp': {
+#             'handlers': ['logfile'],
+#             'level': 'WARNING', # Or maybe INFO or DEBUG
+#             'propagate': False
+#         },
+#     },
+# }
 
 
 # ================================================================= #
