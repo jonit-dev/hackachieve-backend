e253b0663556c4c563431ca51c0e78ac99123a37
Joao Paulo Furtado Silva <joaopaulofurtado@live.com>
2019-01-13 10:21:29 -0800

    changed list

--- rentalmoose/classes/MailchimpHandler.py
+++ rentalmoose/classes/MailchimpHandler.py
@@ -1,4 +1,6 @@
 import json
+from time import sleep
+
 import requests
 from rentalmoose.classes.Environment import *
 
@@ -6,11 +8,11 @@ from rentalmoose.classes.Environment import *
 class MailchimpHandler:
     print("Mailchimp API v3 handler")
 
-    # ENV = Environment.getkey('env')
-    # API_KEY = Environment.getkey('mailchimp_api_key')
+    ENV = Environment.getkey('env')
+    API_KEY = Environment.getkey('mailchimp_api_key')
 
-    ENV = 'prod'
-    API_KEY = "eb8ffce2d2623bb9570702a7aac3afa3-us19"
+    # ENV = 'prod'
+    # API_KEY = "eb8ffce2d2623bb9570702a7aac3afa3-us19"
 
 
     API_URL = 'https://us19.api.mailchimp.com/3.0'
@@ -23,7 +25,7 @@ class MailchimpHandler:
     }
 
     @staticmethod
-    def add_subscriber(email, first_name, last_name, list_id="ec09a29d85"):
+    def add_subscriber(email, first_name, last_name, list_id="0377f23832"):
 
         if MailchimpHandler.ENV == 'dev':
             print('Mailchimp: skipping new subscriber register')
@@ -46,26 +48,29 @@ class MailchimpHandler:
             return response.text
 
     @staticmethod
-    def attach_tags(tags, subscriber, list_id="ec09a29d85"):
+    def attach_tags(tags, subscriber, list_id="0377f23832"):
 
-        if MailchimpHandler.ENV == 'dev':
-            print('Mailchimp: skipping attaching tags to subscriber')
+        if len(tags) > 0: # first of all, if there's some tag to add..
+            if MailchimpHandler.ENV == 'dev':
+                print('Mailchimp: skipping attaching tags to subscriber')
 
-        else:
+            else:
 
-            print("Attaching tags ({}) to user {}".format(tags, subscriber))
+                print("Attaching tags ({}) to user {}".format(tags, subscriber))
 
-            for tag in tags:
-                tag_id = MailchimpHandler.get_tag_id(tag, list_id)
+                for tag in tags:
+                    tag_id = MailchimpHandler.get_tag_id(tag, list_id)
 
-                if tag_id is not False:
-                    MailchimpHandler.attach_tag(tag_id, subscriber, list_id)
-                else:
-                    new_tag_id = MailchimpHandler.create_new_tag(tag, list_id)
-                    MailchimpHandler.attach_tag(new_tag_id, subscriber, list_id)
+                    if tag_id is not False:
+                        sleep(1)
+                        MailchimpHandler.attach_tag(tag_id, subscriber, list_id)
+                    else:
+                        sleep(1)
+                        new_tag_id = MailchimpHandler.create_new_tag(tag, list_id)
+                        MailchimpHandler.attach_tag(new_tag_id, subscriber, list_id)
 
     @staticmethod
-    def create_new_tag(tag_name, list_id="ec09a29d85"):
+    def create_new_tag(tag_name, list_id="0377f23832"):
 
         print("Creating new tag ({}) for list {}".format(tag_name, list_id))
 
@@ -85,7 +90,7 @@ class MailchimpHandler:
         return tag_id
 
     @staticmethod
-    def attach_tag(tag_id, subscriber, list_id="ec09a29d85"):
+    def attach_tag(tag_id, subscriber, list_id="0377f23832"):
 
         print("Attaching tag {} to {} from list {}".format(tag_id, subscriber, list_id))
 
@@ -100,7 +105,7 @@ class MailchimpHandler:
         return response.text
 
     @staticmethod
-    def get_tag_id(name, list_id="ec09a29d85"):
+    def get_tag_id(name, list_id="0377f23832"):
 
         url = '{}/lists/{}/segments'.format(MailchimpHandler.API_URL, list_id)
 
@@ -112,7 +117,7 @@ class MailchimpHandler:
 
         # if tag is found, return its id
         for segment in segments:
-            print("Analyzing segments...found {}".format(segment['name']))
+            # print("Analyzing segments...found {}".format(segment['name']))
             if segment['name'] == name:
                 print("Found tag: {}".format(segment['name']))
                 return segment['id']
