d93a0573d4bc9859ab488ad2885cd337780e0634
fahad khan <fahadkhan6302@gmail.com>
2019-06-28 06:58:42 +0500

    fix-user-detail-issue

--- apps/goals/serializer.py
+++ apps/goals/serializer.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
 from apps.goals.models import Goal, GoalComment, CommentVote
+from apps.users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
 
 
 class GoalSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = Goal
         fields = '__all__'
@@ -16,6 +25,8 @@ class GoalPublicStatusSerializer(serializers.ModelSerializer):
 
 
 class GoalCommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = GoalComment
         exclude = ('timestamp',)
@@ -38,6 +49,7 @@ class GoalCommentUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = GoalComment
         fields = ['text']
+
 
 class GoalOrderSerializer(serializers.ModelSerializer):
     class Meta:
