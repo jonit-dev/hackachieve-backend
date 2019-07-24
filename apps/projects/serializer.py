e88ca7e9904bf65ea909becf838416e16acaf597
fahad khan <fahadkhan6302@gmail.com>
2019-07-24 08:04:42 +0500

    show-labels-when-card-is-loaded

--- apps/projects/serializer.py
+++ apps/projects/serializer.py
@@ -5,6 +5,7 @@ from django.forms.models import model_to_dict
 from apps.boards.models import Board
 from apps.columns.models import Column
 from apps.goals.models import Goal
+from apps.labels.models import Label
 from apps.projects.models import Project
 from apps.users.models import User
 
@@ -13,6 +14,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
+
+
+class LabelContentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Label
+        fields = ['id', 'name']
 
 
 class MemberSerializer(serializers.ModelSerializer):
@@ -52,11 +60,12 @@ class ColumnContentSerializer(serializers.ModelSerializer):
 
 class GoalContentSerializer(serializers.ModelSerializer):
     deadline = serializers.DateTimeField()
+    labels = LabelContentSerializer(many=True)
 
     class Meta:
         model = Goal
         fields = ['id', 'title', 'description', 'deadline', 'order_position', 'duration_hrs', 'priority', 'status',
-                  'is_public']
+                  'is_public', 'labels']
 
 
 class ProjectDetailSerializer(serializers.ModelSerializer):
