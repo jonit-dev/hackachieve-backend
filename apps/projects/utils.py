f4aae41df478da49dd8ba74fc90850ddbb63696d
fahad khan <fahadkhan6302@gmail.com>
2019-07-29 12:01:28 +0500

    update-create-new-project

--- apps/projects/utils.py
+++ apps/projects/utils.py
@@ -2,6 +2,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from apps.boards.models import Board
+from apps.columns.models import Column
+from apps.goals.models import Goal
 from apps.projects.models import Project
 from hackachieve.utils import START_UP_BOARD_LIST
 
@@ -17,5 +19,30 @@ GOAL_STATUS = {
 def create_boards(sender, instance, **kwargs):
     if instance.id and instance.user:
         for item in START_UP_BOARD_LIST:
-            Board.objects.create(name=item['name'], description=item['description'], project_id=instance.id,
-                                 user_id=instance.user.id)
+            board = Board.objects.create(
+                name=item['name'],
+                description=item['description'],
+                project_id=instance.id,
+                user_id=instance.user.id
+            )
+            try:
+                if item['long_term_goal'] and len(item['long_term_goal']) > 0:
+                    for column in item['long_term_goal']:
+                        column_obj = Column.objects.create(
+                            name=column['name'],
+                            board_id=board.id,
+                            user_id=instance.user.id,
+                            description=column['description']
+                        )
+                        if column['short_term_goal'] and len(column['short_term_goal']) > 0:
+                            for goal in column['short_term_goal']:
+                                Goal.objects.create(
+                                    title=goal['title'],
+                                    column_id=column_obj.id,
+                                    user_id=instance.user.id,
+                                    description=goal['description']
+                                )
+            except KeyError:
+                pass
+
+
--- hackachieve/utils.py
+++ hackachieve/utils.py
@@ -1,6 +1,37 @@
 START_UP_BOARD_LIST = [
-    {'name': 'Family', 'description': ''},
-    {'name': 'Health', 'description': ''},
+    {
+        'name': 'Family',
+        'description': '',
+        'long_term_goal': [
+            {
+                'name': 'Long term goal title for Family',
+                'description': '',
+                'short_term_goal': [
+                    {
+                        'title': 'Short term goal title for Family board',
+                        'description': ''
+                    }
+                ]
+            }
+        ],
+
+     },
+    {
+        'name': 'Health',
+        'description': '',
+        'long_term_goal': [
+            {
+                'name': 'Long term goal title for Health board',
+                'description': '',
+                'short_term_goal': [
+                    {
+                        'title': 'Short term goal title for Health board',
+                        'description': ''
+                    }
+                ]
+            }
+        ]
+     },
     {'name': 'Career', 'description': ''},
     {'name': 'Finances', 'description': ''},
     {'name': 'Personal Development', 'description': ''},
