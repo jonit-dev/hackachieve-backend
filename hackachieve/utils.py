e2633e24ccf83ea6b5e2f96ff1223ad2628036ce
Joao Paulo Furtado <joaopaulofurtado@live.com>
2019-08-19 16:07:00 -0700

    default long term goal changes

--- hackachieve/utils.py
+++ hackachieve/utils.py
@@ -7,48 +7,11 @@ goal_deadline = timezone.now() + timedelta(days=2)
 START_UP_BOARD_LIST = [
     {
         'name': 'Family',
-        'description': '',
-        'long_term_goal': [
-            {
-                'name': 'Travel to Vancouver (example)',
-                'description': 'This is a long term goal sample',
-                'deadline': column_deadline,
-                'short_term_goal': [
-                    {
-                        'title': 'Buy air tickets',
-                        'description': 'This is a sample short term goal',
-                        'deadline': timezone.now() + timedelta(days=7)
-                    },
-                    {
-                        'title': 'Hotel reservation',
-                        'description': 'This is a sample short term goal',
-                        'deadline': timezone.now() + timedelta(days=10)
-                    },
-                    {
-                        'title': 'Health insurance',
-                        'description': 'This is a sample short term goal',
-                        'deadline': timezone.now() + timedelta(days=14)
-                    }
-                ]
-            }
-        ],
-
+        'description': ''
     },
     {
         'name': 'Health',
         'description': '',
-        # 'long_term_goal': [
-        #     {
-        #         'name': 'Long term goal title for Health board',
-        #         'description': '',
-        #         'short_term_goal': [
-        #             {
-        #                 'title': 'Short term goal title for Health board',
-        #                 'description': ''
-        #             }
-        #         ]
-        #     }
-        # ]
     },
     {'name': 'Career', 'description': ''},
     {'name': 'Finances', 'description': ''},
@@ -57,8 +20,40 @@ START_UP_BOARD_LIST = [
     {'name': 'Spiritual', 'description': ''},
     {'name': 'Leisure and Fun', 'description': ''},
     {'name': 'Recruitment', 'description': ''},
+    {
+        'name': 'Marketing',
+        'description': '',
+        'long_term_goal': [
+            {
+                'name': 'Get 20 leads/week (example)',
+                'description': 'This is a long term goal sample',
+                'deadline': column_deadline,
+                'short_term_goal': [
+                    {
+                        'title': 'Setup google ads campaign (example)',
+                        'description': 'This is a sample short term goal.',
+                        'deadline': timezone.now() + timedelta(days=7)
+                    },
+                    {
+                        'title': 'Track conversions in google analytics (example)',
+                        'description': 'This is a sample short term goal',
+                        'deadline': timezone.now() + timedelta(days=7)
+                    },
+                    {
+                        'title': 'Hire PPC expert freelancer (example)',
+                        'description': 'This is a sample short term goal',
+                        'deadline': timezone.now() + timedelta(days=10)
+                    },
+                    {
+                        'title': 'Setup mailchimp automation flow',
+                        'description': 'This is a sample short term goal',
+                        'deadline': timezone.now() + timedelta(days=14)
+                    }
+                ]
+            }
+        ],
+    },
     {'name': 'Sales', 'description': ''},
-    {'name': 'Marketing', 'description': ''},
     {'name': 'Operations', 'description': ''},
     {'name': 'Other', 'description': ''},
 ]
